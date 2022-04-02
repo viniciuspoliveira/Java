8acc143662daf45aaf99349d00f05a065fab291e
viniciuspoliveira <viniciusp.penha@gmail.com>
2022-04-02 11:26:49 -0300

    segundo projeto

---
+++ Calculadora.java
@@ -0,0 +1,37 @@
+
+public class Calculadora {
+
+
+	public static void soma (double numero1, double numero2) {
+		
+		double resultado = numero1 + numero2;
+		
+		System.out.println("A soma de " + numero1 + "mais o" + numero2 + "é igual  a" + resultado);
+		
+	}
+	
+	public static void subtracao (double numero1, double numero2) {
+		
+		double resultado = numero1 - numero2;
+		
+		System.out.println("A Subtração de " + numero1 + "menos o" + numero2 + "é igual  a" + resultado);
+		
+	}
+	public static void multiplicacao (double numero1, double numero2) {
+		
+		double resultado = numero1 * numero2;
+		
+		System.out.println("A Multiplicação do " + numero1 + "vezes o" + numero2 + "é igual  a" + resultado);
+		
+	}
+	public static void divisao (double numero1, double numero2) {
+		
+		double resultado = numero1 / numero2;
+		
+		System.out.println("A Multiplicação do " + numero1 + "vezes o" + numero2 + "é igual  a" + resultado);
+		
+	}
+
+
+
+}
